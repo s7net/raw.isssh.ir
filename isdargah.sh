5ec4d56f8d41dc1d3a99b98634babb8c31f97d4b
Amir Hossein Soleimani <info@s7net.ir>
2025-12-17 10:18:36 +0330

    Update isdargah.sh

--- isdargah.sh
+++ isdargah.sh
@@ -20,7 +20,7 @@ gateways=(
   "Refah_Bank ref.sayancard.ir"
   "Ghesta api.ghesta.ir"
   "OmidPay say.shaparak.ir"
-  "Zarinpal www.zarinpal.com"
+  "Zarinpal payment.zarinpal.com"
   "IranKish ikc.shaparak.ir"
   "BehPardakht bpm.shaparak.ir"
   "Parsian pec.shaparak.ir"
